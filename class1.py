4ed27b8f5b0f76fad73a2e193e79b835d8f7c4d7
naveen rasipogula <naveennaveen62318@gmail.com>
2025-04-13 02:51:17 +0530

    main

--- class1.py
+++ class1.py
@@ -1,12 +1,43 @@
-#conditional statements (if-elif-else)
-# if statement is correct ,then will excute statement
-# if statement is condition not correct,then elif or esle statement will execute
-# if and elif statements conditional are not correct ,then else statement execute
-# program1
-# Two numbers program
-num1=12
-num2=15
-if num1<num2:
-    print("Hello world!")
-else:
-    print("excute else")
+# Python is high level and general purpose programming and scripting language
+# python was created 1987
+# Python was Developed by guido van rossum in 1991.
+# Python is used for Software developemnt, web applications ,Machine learning and Atificial intelligence,DataScice,AWS or Cloud,Game appilications
+# syntax:-
+print("Hello Python")
+# Comment is # symbol
+# python is Scripting ,functional,object oriented programming way.
+# python indentations are represents space
+# indentation must 4 digit space
+# example
+# def mydata():
+#     print("Python")
+# nydata()
+
+# variable
+# variables are must be starts with letter or underscore(_)
+# variable can not starts with a number 
+
+myname="Hello"
+my_name="python"
+myName="hello"
+MYNUM="Helo"
+my_Name="Python"
+Myname="Python3"
+myname34="world1"
+MYNUM34="Hello"
+myName34="python3"
+my_Name23="Advance Python"
+print(myname)
+print(my_name)
+print(myName)
+print(MYNUM)
+print(my_Name)
+print(Myname)
+print(myName34)
+print(MYNUM34)
+print(my_Name23)    
+# we don't write below variable declarations
+# 4myname="hello" #don't starts with number
+# My name="Hello" #don't give space
+# My-Name="Hello" #don't give hypen(-)symbol
+# print(My-Name)
